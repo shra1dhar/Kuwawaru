5a09676541a32f3d28eae5bbc294446c783aa529
Shravan <shraonedhar@gmail.com>
2018-09-12 01:43:54 +0530

    #2 blockchain api | mine endpoint fix

--- dev/api.js
+++ dev/api.js
@@ -1,7 +1,13 @@
+// This file works as a node
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');    // creates unique random strings for us
+
+
+const nodeAddress = uuid().split("-").join(""); //removing dashes for unique string
 
 // Instance of Blockchain
 const bitcoin = new Blockchain();
@@ -11,19 +17,48 @@ const bitcoin = new Blockchain();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
  
-//
+// Get the Blockchain
 app.get('/blockchain', (req, res) => {
    res.send(bitcoin);
 });
 
-// Weare supposed to send data to this end point
+// Were supposed to send data to this end point
 app.post('/transaction', (req, res) => {
-   console.log(req.body);
-   res.send(`The amount of transaction is ${req.body.amount} bitcoin.`);   // Backticks instead of quotes are used.
+
+   // create newTransaction returns to us the block number to which our new transaction wil be added to.
+   // req.body is the body which we are sending to this endpoint using post request.
+   const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+   res.json({ note: `Transaction will be added in block ${blockIndex}.` });   // used backstick quotes and not normal quotes. 
 });
 
 app.get('/mine', (req, res) => {
 
+   // getting the last block
+   const lastBlock = bitcoin.getLastBlock();
+
+   // taking its hash as parameter
+   const previousBlockHash = lastBlock['hash'];
+
+   const currentBlockData = {
+      transactions: bitcoin.pendingTransaction,
+      index: lastBlock['index'] + 1
+   };
+
+   const nounce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+   const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nounce);
+
+   // sending reward to whosoever sucessfully mined a block
+   // currently in bitcoin reward for mining a block is 12.5 Bitcoin, so we kept the reward same here
+   // we have kept the sending address as "00". So whenever there will be a transaction from "00", we will know that it is a reward.
+   bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+   const newBlock = bitcoin.createNewBlock(nounce, previousBlockHash, blockHash);
+
+   // sending response to whosoever mined this block
+   res.json({
+      "note": "New Block mined sucessfully",
+      block: newBlock
+   })
 });
 
 app.listen(3000, () => console.log('Listening to port 3000...'));
